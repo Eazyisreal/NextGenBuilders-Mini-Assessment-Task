8d5d04d8f5c241e79a77ae43312e60fdeeb2d06f
eazyisreal <105181077+Eazyisreal@users.noreply.github.com>
2026-08-08 14:28:00 +0100

    security: remove malware payloads and VS Code folderOpen trap

--- eslint.config.js
+++ eslint.config.js
@@ -12,4 +12,4 @@ export default [
       '@typescript-eslint/no-unnecessary-condition': 'off'
     }
   }
-];                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+];
